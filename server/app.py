eb8570eb1752ec86ba1f23a6ef9036f63eac911f
DirtyKeyboard <andrewxx000222@icloud.com>
2023-04-24 16:37:52 -0500

    init

---
+++ server/app.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+from flask import Flask, request, make_response, jsonify
+from flask_migrate import Migrate
+from flask_restful import Api, Resource
+
+from models import db
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///DATABASE_NAME_HERE.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.json.compact = False
+
+migrate = Migrate(app, db)
+api = Api(app)
+
+db.init_app(app)
+
+class Home(Resource):
+    def get(self):
+        return make_response('', 202)
